1d74ae949ac6b183c6c828b46ec265e9b774b47a
Sakthi Narayanan S <160324737+ArchanaSakthi1702@users.noreply.github.com>
2026-03-01 21:34:09 +0530

    Add production origin to CORS settings

--- app/main.py
+++ app/main.py
@@ -37,7 +37,8 @@ origins = [
     "http://localhost:3000",   # React Dev
     "http://127.0.0.1:3000",
     "http://localhost:5173",   # Vite
-    "http://127.0.0.1:5173"
+    "http://127.0.0.1:5173",
+     "https://convocationadminpanel.vercel.app"
 ]
 
 app = FastAPI(
